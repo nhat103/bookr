628182b7bf4ca08ad2b4cd8f87c716d4da900077
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-10-06 13:26:45 +0700

    chapter2

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 
@@ -8,6 +9,9 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50, help_text="the name of publisher")
     website = models.URLField(help_text="the publisher's website")
     email = models.EmailField(help_text="the publisher's email")
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -21,6 +25,9 @@ class Book(models.Model):
     contributors = models.ManyToManyField(
         'Contributor', through="BookContributor")
 
+    def __str__(self):
+        return self.title
+
 
 class Contributor(models.Model):
     # A contributor to a Book, e.g. author, editor, \co-author
@@ -30,3 +37,31 @@ class Contributor(models.Model):
         max_length=50, help_text="The contributor's last name or name")
     email = models.EmailField(
         help_text="The contact email for the contributor.")
+
+    def __str__(self):
+        return self.first_name
+
+
+class BookContributor(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(verbose_name="The role this contributor had in the book.",
+                            choices=ContributionRole.choices, max_length=25)
+
+
+class Review(models.Model):
+    content = models.TextField(help_text="the review text")
+    rating = models.IntegerField(
+        help_text="the rating the  reviewer has given")
+    date_created = models.DateTimeField(
+        auto_now_add=True, help_text="the day and time review was created")
+    date_edit = models.DateTimeField(
+        null=True, help_text="date and time review was last edit")
+    creator = models.ForeignKey(
+        auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, help_text="The Book that this review is for")
--- reviews/views.py
+++ reviews/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     name = "Welcom to bookr"
     return render(request, "base.html", {'name': name})
+
+
+def welcome_view(request):
+    return render(request, 'base.html')
